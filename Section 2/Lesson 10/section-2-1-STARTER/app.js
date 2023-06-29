2bce22629ce3bdc2e6b4c018e44bdf5a7e6e7861
harithava <134498340+harithava@users.noreply.github.com>
2023-06-29 14:05:57 +0100

    HH14: Methods.

--- Section 2/Lesson 10/section-2-1-STARTER/app.js
+++ Section 2/Lesson 10/section-2-1-STARTER/app.js
@@ -2,21 +2,17 @@ const vm = Vue.createApp({
     data() {
         return {
             firstName: 'John',
-            name: "Doe"
+            lastName: "Doe"
+        }
+    },
+    methods: {
+        fullName() {
+            return `${this.firstName} ${this.lastName.toUpperCase()}`;
         }
     }
 }).mount('#app')
 
 
-setTimeout(() => {
-    vm.firstName = 'Bob';
-}, 2000)
-
-// Vue.createApp({
-//     data() {
-//         return {
-//             firstName: 'Jane',
-//             name: "Doe"
-//         }
-//     }
-// }).mount('#app1')
+// setTimeout(() => {
+//     vm.firstName = 'Bob';
+// }, 2000)
