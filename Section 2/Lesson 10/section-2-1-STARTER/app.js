07e89844afdc57a6345d619cf973e720ff23b227
harithava <134498340+harithava@users.noreply.github.com>
2023-06-29 13:29:02 +0100

    HH13: Creating Multiple Vue instance.

--- Section 2/Lesson 10/section-2-1-STARTER/app.js
+++ Section 2/Lesson 10/section-2-1-STARTER/app.js
@@ -6,3 +6,11 @@ Vue.createApp({
         }
     }
 }).mount('#app')
+Vue.createApp({
+    data() {
+        return {
+            firstName: 'Jane',
+            name: "Doe"
+        }
+    }
+}).mount('#app1')
