d165de754ca6deedca16d57392e380c2e8e7d632
harithava <134498340+harithava@users.noreply.github.com>
2023-07-03 00:39:46 +0100

    HH70: Dynamic Components.

---
+++ Section 7/Lesson 70/dynamic-components/src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Home Page</h1>
+</template>
+<script>
+export default { 
+    unmounted() {
+        console.log('Home Component unmounted.')
+    },
+    activated() {
+        console.log('Home Component activated.')
+    },
+    deactivated() {
+        console.log('Home Component deactivated.')
+    }
+}
+</script>
