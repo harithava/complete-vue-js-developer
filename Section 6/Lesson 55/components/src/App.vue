6d3ca751960a731a2468777b209c811c359d22c1
harithava <134498340+harithava@users.noreply.github.com>
2023-07-02 12:47:31 +0100

    HH64: Limitation of Props.

--- Section 6/Lesson 55/components/src/App.vue
+++ Section 6/Lesson 55/components/src/App.vue
@@ -1,14 +1,24 @@
 <template>
-  <p>Hey!</p>
-  <Greeting></Greeting>
+  <h3>Hey!</h3>
+  <button type="button" @click="age++" >Update Age</button>
+  <hr/>
+  <greeting :age="age"></greeting>
+  <user :age="age"></user>
 </template>
 <script>
 import Greeting from '@/components/Greeting.vue'
+import User from '@/components/User.vue'
 
 export default {
   name: 'App',
   components: {
-     Greeting
+     Greeting,
+     User
+  },
+  data() {
+    return {
+      age: 20
+    }
   }
 }
 </script>
