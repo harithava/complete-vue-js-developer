a513dc9d98ef5c89aeea4f0b514f90345a418d2a
harithava <134498340+harithava@users.noreply.github.com>
2023-07-07 15:15:33 +0100

    HH116:Side Bar Slot properties. V-SLOT

--- Section 10/Lesson 116/section-10-12-STARTER/src/App.vue
+++ Section 10/Lesson 116/section-10-12-STARTER/src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-    <hello-world #default="{ user, favorites }">
+    <!-- <hello-world #default="{ user, favorites }">
       <p>Hello {{ user.name }}. I like {{ favorites[0] }}.</p>
+    </hello-world> -->
+    <hello-world v-slot="v">
+      <p>Hello {{ v.user.name }}</p>
     </hello-world>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
