997a01b2fba2bc2cb27255c784daa34b402d62b4
harithava <134498340+harithava@users.noreply.github.com>
2023-07-11 00:04:05 +0100

    HH132: Logging the user in after signup.

--- Section 11/music/src/components/RegisterForm.vue
+++ Section 11/music/src/components/RegisterForm.vue
@@ -108,6 +108,8 @@
 
 <script>
 import { auth, usersCollection } from "@/includes/firebase";
+import { mapWritableState } from "pinia";
+import useUserStore from "@/stores/user";
 
 export default {
   name: "RegisterForm",
@@ -134,6 +136,9 @@ export default {
       reg_alert_variant: "bg-blue-500",
       reg_alert_msg: "Please wait! Your account is being created.",
     };
+  },
+  computed: {
+    ...mapWritableState(useUserStore, ["userLoggedIn"]),
   },
   methods: {
     async register(values) {
@@ -173,6 +178,8 @@ export default {
           "An unexpected error occured. Please try again later.";
         return;
       }
+
+      this.userLoggedIn = true;
 
       this.reg_alert_variant = "bg-green-500";
       this.reg_alert_msg = "Success! Your account has been created.";
