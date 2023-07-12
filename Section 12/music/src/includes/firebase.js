c7cf3a06ed00c51cd6f0fe7bd910ecfecd4bae29
harithava <134498340+harithava@users.noreply.github.com>
2023-07-12 08:12:04 +0100

    HH140: Section12 Started

---
+++ Section 12/music/src/includes/firebase.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCnd1DAmb4sCHjicljwT-Z4fjdkJdb3vn8",
+  authDomain: "music-6bb45.firebaseapp.com",
+  projectId: "music-6bb45",
+  storageBucket: "music-6bb45.appspot.com",
+  appId: "1:710750631860:web:465d66d9dc5f1f74f6e4e1",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const auth = firebase.auth();
+const db = firebase.firestore();
+
+const usersCollection = db.collection("users");
+
+export { auth, db, usersCollection };
